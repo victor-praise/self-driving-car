ec1a64069c6741092e2a20220291c198fa648822
victor-praise <vpraise27@gmail.com>
2022-08-30 19:41:04 -0400

    cars can move between lanes

--- car.js
+++ car.js
@@ -13,7 +13,7 @@ class Car{
         this.controls = new Controls();
     }
 
-    update(){
+    #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration;
             
@@ -51,6 +51,9 @@ class Car{
     }
         this.x -=Math.sin(this.angle) * this.speed;
         this.y -=Math.cos(this.angle) * this.speed;
+    }
+    update(){
+       this.#move(); 
     }
     draw(ctx){
         ctx.save();
